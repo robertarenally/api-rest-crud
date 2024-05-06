91eaf223f7a3bf1f53cb5e077b252b68ef59db3f
Roberta R S Melo <J96858@amcontratos.com.br>
2024-05-06 08:37:55 -0300

    Implementando os testes unitários do UsuarioService

--- src/test/java/br/com/gestao/services/UsuarioServiceTest.java
+++ src/test/java/br/com/gestao/services/UsuarioServiceTest.java
@@ -46,13 +46,16 @@ class UsuarioServiceTest {
 	private ModelMapper modelMapper;
 
 	Endereco endereco;
+	EnderecoDTO enderecoDTO;
 	List<Endereco> enderecos = new ArrayList<>();
+	List<EnderecoDTO> enderecosDTO = new ArrayList<>();
 	List<Endereco> enderecosEmpty = new ArrayList<>();
 	
 	private Page<Endereco> pageEndereco;
 	private Page<Endereco> pageEnderecoEmpty;
 	
 	Usuario usuario;
+	UsuarioDTO usuarioDTO;
 	List<Usuario> usuarios = new ArrayList<>();
 	List<Usuario> usuariosEmpty = new ArrayList<>();
 	
@@ -65,11 +68,15 @@ class UsuarioServiceTest {
 		// monta a resposta do repository
 		endereco = Endereco.builder().id(1L).cep("59248970").logradouro("Estrada do Cajueiro, s/n").numero("168")
 				.estado("RN").cidade("Lagoa Salgada").build();
+		enderecoDTO = EnderecoDTO.builder().id(1L).cep("59248970").logradouro("Estrada do Cajueiro, s/n").numero("168")
+				.estado("RN").cidade("Lagoa Salgada").build();
 		enderecos.add(endereco);
+		enderecosDTO.add(enderecoDTO);
 		pageEndereco = new PageImpl<>(enderecos);
 		pageEnderecoEmpty = new PageImpl<>(enderecosEmpty);
 		
 		usuario = Usuario.builder().id(1L).nome("Manoel Rafael Osvaldo Assis").enderecos(enderecos).build();
+		usuarioDTO = UsuarioDTO.builder().id(1L).nome("Manoel Rafael Osvaldo Assis").enderecos(enderecosDTO).build();
 		usuarios.add(usuario);
 		
 		pageUsuario = new PageImpl<>(usuarios);
@@ -281,5 +288,199 @@ class UsuarioServiceTest {
 
 		// Verificação do resultado
 		assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+	}
+	
+	@Test
+	void testeSalvarUsuarioComSucesso() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		when(usuarioRepository.save(Mockito.any())).thenReturn(usuario);
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Usuario.class))).thenAnswer(invocation -> {
+			UsuarioDTO usuario1 = invocation.getArgument(0);
+			EnderecoDTO endereco1 = usuario1.getEnderecos().get(0);
+			Endereco endereco2 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			List<Endereco> enderecos2 = new ArrayList<>();
+			enderecos2.add(endereco2);
+			Usuario usuario2 = Usuario.builder().id(usuario1.getId()).nome(usuario1.getNome())
+					.enderecos(enderecos2).build();
+			return usuario2;
+		});
+				
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(UsuarioDTO.class))).thenAnswer(invocation -> {
+			Usuario usuario1 = invocation.getArgument(0);
+			Endereco endereco1 = usuario1.getEnderecos().get(0);
+			EnderecoDTO enderecoDTO = EnderecoDTO.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			List<EnderecoDTO> enderecos = new ArrayList<>();
+			enderecos.add(enderecoDTO);
+			UsuarioDTO usuarioDTO = UsuarioDTO.builder().id(usuario1.getId()).nome(usuario1.getNome())
+					.enderecos(enderecos).build();
+			return usuarioDTO;
+		});
+
+		// Execução do método a ser testado
+		ResponseEntity<ResponseWrapper<UsuarioDTO>> responseEntity = usuarioService.salvarUsuario(usuarioDTO);
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+		assertEquals(usuarioDTO.getId(), responseEntity.getBody().getData().getId());
+		assertEquals(null, responseEntity.getBody().getMessage());
+	}
+	
+	@Test
+	void testeSalvarUsuarioComErro() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		Usuario usuario3 = Usuario.builder().build();
+		when(usuarioRepository.save(Mockito.any())).thenReturn(usuario3);
+		Endereco endereco3 = Endereco.builder().build();
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco3);
+		
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Usuario.class))).thenAnswer(invocation -> {
+			UsuarioDTO usuario1 = invocation.getArgument(0);
+			EnderecoDTO endereco1 = usuario1.getEnderecos().get(0);
+			Endereco endereco2 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			List<Endereco> enderecos2 = new ArrayList<>();
+			enderecos2.add(endereco2);
+			Usuario usuario2 = Usuario.builder().id(usuario1.getId()).nome(usuario1.getNome())
+					.enderecos(enderecos2).build();
+			return usuario2;
+		});
+
+		// Execução do método a ser testado
+		ResponseEntity<ResponseWrapper<UsuarioDTO>> responseEntity = usuarioService.salvarUsuario(usuarioDTO);
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+	}
+	
+	@Test
+	void testeSalvarEnderecoDeUsuarioComSucesso() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		when(usuarioRepository.findById(Mockito.any())).thenReturn(Optional.of(usuario));
+		when(enderecoRepository.findByUsuarioId(Mockito.any())).thenReturn(enderecos);
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Endereco.class))).thenAnswer(invocation -> {
+			EnderecoDTO endereco1 = invocation.getArgument(0);
+			Endereco endereco3 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return endereco3;
+		});
+		
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(EnderecoDTO.class))).thenAnswer(invocation -> {
+			Endereco endereco1 = invocation.getArgument(0);
+			EnderecoDTO enderecoDTO = EnderecoDTO.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return enderecoDTO;
+		});
+				
+		// Execução do método a ser testado
+		ResponseEntity<ResponseWrapper<EnderecoDTO>> responseEntity = usuarioService.salvarEndereco(enderecoDTO, usuario.getId());
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+		assertEquals(enderecoDTO.getId(), responseEntity.getBody().getData().getId());
+		assertEquals(null, responseEntity.getBody().getMessage());
+	}
+	
+	@Test
+	void testeSalvarEnderecoDeUsuarioComErroDeUsuarioNaoEncontrado() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		when(usuarioRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+		when(enderecoRepository.findByUsuarioId(Mockito.any())).thenReturn(enderecos);
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Endereco.class))).thenAnswer(invocation -> {
+			EnderecoDTO endereco1 = invocation.getArgument(0);
+			Endereco endereco3 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return endereco3;
+		});
+				
+		// Execução do método a ser testado
+		ResponseEntity<ResponseWrapper<EnderecoDTO>> responseEntity = usuarioService.salvarEndereco(enderecoDTO, usuario.getId());
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+	}
+	
+	@Test
+	void testeSalvarEnderecoDeUsuarioComErro() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		when(usuarioRepository.findById(Mockito.any())).thenReturn(Optional.of(usuario));
+		when(enderecoRepository.findByUsuarioId(Mockito.any())).thenReturn(enderecos);
+		Endereco endereco4 = Endereco.builder().build();
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco4);
+
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Endereco.class))).thenAnswer(invocation -> {
+			EnderecoDTO endereco1 = invocation.getArgument(0);
+			Endereco endereco3 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return endereco3;
+		});
+				
+		// Execução do método a ser testado
+		ResponseEntity<ResponseWrapper<EnderecoDTO>> responseEntity = usuarioService.salvarEndereco(enderecoDTO, usuario.getId());
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+	}
+	
+	@Test
+	void testeSalvarEnderecoDeUsuarioComoPrincipalComSucesso() throws Exception {
+
+		// Configurar comportamento simulado do repository
+		when(usuarioRepository.findById(Mockito.any())).thenReturn(Optional.of(usuario));
+		when(enderecoRepository.findByUsuarioId(Mockito.any())).thenReturn(enderecos);
+		when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(Endereco.class))).thenAnswer(invocation -> {
+			EnderecoDTO endereco1 = invocation.getArgument(0);
+			Endereco endereco3 = Endereco.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return endereco3;
+		});
+		
+		// Configuração do comportamento simulado do ModelMapper
+		when(modelMapper.map(any(), eq(EnderecoDTO.class))).thenAnswer(invocation -> {
+			Endereco endereco1 = invocation.getArgument(0);
+			EnderecoDTO enderecoDTO = EnderecoDTO.builder().id(endereco1.getId()).cep(endereco1.getCep())
+					.logradouro(endereco1.getLogradouro()).numero(endereco1.getNumero()).estado(endereco1.getEstado())
+					.cidade(endereco1.getCidade()).principal(endereco1.getPrincipal()).build();
+			return enderecoDTO;
+		});
+				
+		// Execução do método a ser testado
+		enderecoDTO.setPrincipal(true);
+		ResponseEntity<ResponseWrapper<EnderecoDTO>> responseEntity = usuarioService.salvarEndereco(enderecoDTO, usuario.getId());
+
+		// Verificação do resultado
+		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+		assertEquals(enderecoDTO.getId(), responseEntity.getBody().getData().getId());
+		assertEquals(null, responseEntity.getBody().getMessage());
 	}
 }
